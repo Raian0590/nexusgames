e09f3a5446c3ba0c71aefb9ce17f6f32dc39a779
Raian Emanuel Uchôa Laurentino <raian.pro9@gmail.com>
2025-05-19 23:58:15 -0300

    Project created

---
+++ src/main/java/com/raian/NexusGames/NexusGamesApplication.java
@@ -0,0 +1,13 @@
+package com.raian.NexusGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raian/NexusGames/NexusGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raian.NexusGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
