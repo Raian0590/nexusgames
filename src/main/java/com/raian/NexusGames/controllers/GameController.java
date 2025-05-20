07abdf080b67029db1dbbec175474f66f5338807
Raian Emanuel Uchôa Laurentino <raian.pro9@gmail.com>
2025-05-20 17:12:16 -0300

    feat: adiciona endpoint para buscar jogo por ID

--- src/main/java/com/raian/NexusGames/controllers/GameController.java
+++ src/main/java/com/raian/NexusGames/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.raian.NexusGames.controllers;
 
+import com.raian.NexusGames.dto.GameDTO;
 import com.raian.NexusGames.dto.GameMinDTO;
 import com.raian.NexusGames.entities.Game;
 import com.raian.NexusGames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,13 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
+
     @GetMapping
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 }
--- src/main/java/com/raian/NexusGames/services/GameService.java
+++ src/main/java/com/raian/NexusGames/services/GameService.java
@@ -1,11 +1,14 @@
 package com.raian.NexusGames.services;
 
+import com.raian.NexusGames.dto.GameDTO;
 import com.raian.NexusGames.dto.GameMinDTO;
 import com.raian.NexusGames.entities.Game;
 import com.raian.NexusGames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
 @Service
@@ -14,9 +17,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional (readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional (readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-        return dto;
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
